cf2bace8336b9857cccec369995256b3569f65a1
Pokydko Alex <Sashk-pokudko@outlook.com>
2017-12-11 12:53:48 +0200

    fixed add function

--- student.cpp
+++ student.cpp
@@ -6,9 +6,8 @@ student::student()
 }
 
 student::student(QString _name, QString _sname, QString _tname, bool _sex, int _age, int _bday, int _bmounth, int _byear, QString _phone_number, QString _adress, bool _type_of_education):
-    type_of_education(_type_of_education)
+    type_of_education(_type_of_education), person( _name, _sname, _tname, _sex, _age, _bday, _bmounth, _byear, _phone_number, _adress)
 {
-    person( _name, _sname, _tname, _sex, _age, _bday, _bmounth, _byear, _phone_number, _adress);
 }
 
 void student::addStudentToDB()
